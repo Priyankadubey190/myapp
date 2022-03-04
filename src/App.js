e94f7974666d736a6d3b2cdffaf7c294188e5db2
Priyankadubey190 <priyankadubey0811@gmail.com>
2022-03-04 18:18:32 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="add-note" type="text">
+       <div >hello</div> 
+       <div >welcome to masai</div> 
+        </div>
     </div>
   );
 }
